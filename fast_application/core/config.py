73e9ec7de3dde567789584b110ed2c588910f24e
Romberto <korhoff@mail.ru>
2025-06-06 17:01:29 +0400

    added: db_helper

--- fast_application/core/config.py
+++ fast_application/core/config.py
@@ -1,9 +1,16 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, PostgresDsn
 from pydantic_settings import BaseSettings
 
 class RunConfig(BaseModel):
     host: str = "0.0.0.0"
     port: int = 8000
+
+class DataBaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size:int = 50
+    max_overflow: int = 10
 
 class ApiPrefix(BaseModel):
     prefix:str = '/api'
@@ -12,6 +19,7 @@ class ApiPrefix(BaseModel):
 class Settings(BaseSettings):
     run:RunConfig = RunConfig()
     api:ApiPrefix = ApiPrefix()
+    db: DataBaseConfig
 
 
 settings = Settings()
---
+++ fast_application/models/db_helper.py
@@ -0,0 +1,24 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine, async_sessionmaker
+
+from fast_application.core.config import settings
+
+
+class DataBaseHelper:
+    def __init__(self, url: str, echo: bool = True, echo_pool: bool = True, pool_size: int = 5, max_overflow: int = 10):
+        self.engine: AsyncEngine = create_async_engine(
+            url=url, echo=echo, echo_pool=echo_pool, pool_size=pool_size, max_overflow=max_overflow, )
+
+        self.session_factory = async_sessionmaker(
+            bind=self.engine, autoflush=False, autocommit=False, expire_or_commit=False, )
+
+    async def dispose(self):
+        await self.engine.dispose()
+
+    async def session_getter(self):
+        async with self.session_factory() as session:
+            yield session
+
+
+db_helper = DataBaseHelper(
+    url=str(settings.db.url), echo=settings.db.echo, echo_pool=settings.db.echo_pool, pool_size=settings.db.pool_size,
+    max_overflow=settings.db.max_overflow, )
