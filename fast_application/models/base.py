7580fd35648c38bf2474802fd38cbeadc0c1f070
Romberto <korhoff@mail.ru>
2025-06-09 11:52:15 +0400

    first alembic migration

--- fast_application/models/base.py
+++ fast_application/models/base.py
@@ -1,12 +1,11 @@
 import uuid
-
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
 from sqlalchemy.dialects.postgresql import UUID
 
 class Base(DeclarativeBase):
     __abstract__ = True
 
-    id = Mapped[uuid.UUID] = mapped_column(
+    id: Mapped[uuid.UUID] = mapped_column(
         UUID(as_uuid=True),
         primary_key=True,
         default=uuid.uuid4
