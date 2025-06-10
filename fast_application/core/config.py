2dc49e338f35e7f7540f88c267a70b798f40aa5d
Romberto <korhoff@mail.ru>
2025-06-10 21:08:19 +0400

    db config

--- fast_application/core/config.py
+++ fast_application/core/config.py
@@ -1,17 +1,28 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, PostgresDsn
 from pydantic_settings import BaseSettings
+
 
 class RunConfig(BaseModel):
     host: str = "0.0.0.0"
     port: int = 8000
 
+
 class ApiPrefix(BaseModel):
-    prefix:str = '/api'
+    prefix: str = '/api'
+
+
+class DataBaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    max_overflow: int = 10,
+    pool_size: int = 50
 
 
 class Settings(BaseSettings):
-    run:RunConfig = RunConfig()
-    api:ApiPrefix = ApiPrefix()
+    run: RunConfig = RunConfig()
+    api: ApiPrefix = ApiPrefix()
+    db: DataBaseConfig
 
 
 settings = Settings()
--- fast_application/main.py
+++ fast_application/main.py
@@ -3,12 +3,23 @@ import uvicorn
 from fastapi import FastAPI
 from api import router as api_router
 from fast_application.core.config import settings
+from models.db_helper import db_helper
 
-app = FastAPI()
-app.include_router(api_router, prefix=settings.api.prefix)
+
+def lifespan(app:FastAPI):
+    yield app
+    db_helper.dispose()
+
+
+
+
+app_main = FastAPI(
+    lifespan=lifespan()
+    )
+app_main.include_router(api_router, prefix=settings.api.prefix)
 
 if __name__ == "__main__":
-    uvicorn.run('main:app',
+    uvicorn.run('main:app_main',
                 host = settings.run.host,
                 port= settings.run.port,
                 reload=True)
