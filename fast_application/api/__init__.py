7a4f4963190da1e8f242a09fa73c7ff66891eced
Romberto <korhoff@mail.ru>
2025-06-20 01:32:28 +0400

    refactor nginx8

--- fast_application/api/__init__.py
+++ fast_application/api/__init__.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
-from fastapi.responses import JSONResponse
 
-from fast_application.core.config import settings
+from core.config import settings
 from .v1 import router as v1_router
 
 router = APIRouter(
