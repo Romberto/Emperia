de4f6313fdda1da0121cecac80f7c2a71bf38670
Romberto <korhoff@mail.ru>
2025-06-19 20:03:45 +0400

    init back prod

--- react_vite/src/components/page/Festivals.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Festivals: React.FC = () => {
-  return (
-    <div>
-        <h2 style={{color: "white", fontSize: "20px"}}>Здесь будут фестивали</h2>
-    </div>
-  );
-};
