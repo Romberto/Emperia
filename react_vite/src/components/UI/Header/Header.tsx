2359e47f32fbaab07c45449f68759b0cd1a8aa27
Romberto <korhoff@mail.ru>
2025-06-20 04:04:22 +0400

    refactor front CORS

--- react_vite/src/components/UI/Header/Header.tsx
+++ react_vite/src/components/UI/Header/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "./Header.module.css";
 import logo from "../../../assets/moto.png";
 import TelegramLoginButton from "../TelegramButton/TelegramButton";
-import axios from 'axios';
+import axios from "axios";
 
 type UserTelegramType = {
   id: number;
@@ -15,7 +15,29 @@ type UserTelegramType = {
 
 export const Header: React.FC = () => {
   const handleTelegramAuth = async (user: UserTelegramType) => {
-    
+    try {
+      console.log("Пользователь Telegram авторизован!!!:", user);
+
+      const response = await axios.post(
+        "https://cafeapi.ru/api/v1/auth/telegram/login",
+        user,
+        {
+          withCredentials: true, // важно, если сервер ставит cookie
+        }
+      );
+
+      const { access_token } = response.data;
+
+      if (access_token) {
+        localStorage.setItem("access_token", access_token);
+      }
+
+
+
+      console.log("Авторизация успешна");
+    } catch (error) {
+      console.error("Ошибка при Telegram авторизации:", error);
+    }
   };
   return (
     <header>
