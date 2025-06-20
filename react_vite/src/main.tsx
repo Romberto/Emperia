0bdada20e4faca911158e723d6b1ca8b0cf57e97
Romberto <korhoff@mail.ru>
2025-06-20 13:23:34 +0400

    пробуем подключить redux1

--- react_vite/src/main.tsx
+++ react_vite/src/main.tsx
@@ -4,7 +4,7 @@ import { HashRouter  } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
-import { store } from "./app/store.tsx";
+import { store } from "./app/store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
