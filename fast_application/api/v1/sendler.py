a329f383f4614bf19f57a8f41a3f8174a324b678
Romberto <korhoff@mail.ru>
2025-06-26 15:56:34 +0400

    refactor send/sos'

--- fast_application/api/v1/sendler.py
+++ fast_application/api/v1/sendler.py
@@ -21,25 +21,29 @@ CHAT_ID = settings.chat_id
 async def send_sos(data: SosRequest, payload: dict = Depends(_get_current_payload),
                    session: AsyncSession = Depends(db_helper.session_getter)):
     user: UserBase = await _get_current_user(session=session, payload=payload)
-    situation_text = {
-        "dtp": "🚗 ДТП", "conflict": "⚠️ Конфликтная ситуация",
-        }.get(data.type, "❓ Неизвестная ситуация")
-
-    text = (f"<b>SOS сигнал!</b>\n"
-            f"📝 Ситуация: {situation_text}\n"
-            f"🌍 Координаты: "
-            f"<a href='https://maps.google.com/?q={data.latitude},{data.longitude}'>Открыть карту</a>\n")
-
-    if user.username:
-        text = f"👤 @{user.username}\n" + text
-
-    async with httpx.AsyncClient() as client:
-        response = await client.post(
-            f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage", json={
-                "chat_id": CHAT_ID, "text": text, "parse_mode": "HTML", "disable_web_page_preview": True,
-                }, )
-
-    if response.status_code != 200:
-        return {"message": "Ошибка отправки", "error": response.text}
-
-    return {"message": "Сообщение отправлено"}
+    if user:
+        return {"user":user}
+    else:
+        return {"message": "not user"}
+    # situation_text = {
+    #     "dtp": "🚗 ДТП", "conflict": "⚠️ Конфликтная ситуация",
+    #     }.get(data.type, "❓ Неизвестная ситуация")
+    #
+    # text = (f"<b>SOS сигнал!</b>\n"
+    #         f"📝 Ситуация: {situation_text}\n"
+    #         f"🌍 Координаты: "
+    #         f"<a href='https://maps.google.com/?q={data.latitude},{data.longitude}'>Открыть карту</a>\n")
+    #
+    # if user.username:
+    #     text = f"👤 @{user.username}\n" + text
+    #
+    # async with httpx.AsyncClient() as client:
+    #     response = await client.post(
+    #         f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage", json={
+    #             "chat_id": CHAT_ID, "text": text, "parse_mode": "HTML", "disable_web_page_preview": True,
+    #             }, )
+    #
+    # if response.status_code != 200:
+    #     return {"message": "Ошибка отправки", "error": response.text}
+    #
+    # return {"message": "Сообщение отправлено"}
