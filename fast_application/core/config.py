5a50e8b288471a6824aaca5d4e1037e1e8de7f5f
Romberto <korhoff@mail.ru>
2025-07-01 23:34:08 +0400

    refactor time expire

--- fast_application/core/config.py
+++ fast_application/core/config.py
@@ -37,8 +37,8 @@ class AuthJWT(BaseModel):
     private_key_path: Path = BASE_DIR / "keys" / "jwt-private.pem"
     public_key_path:Path = BASE_DIR / "keys" / "jwt-public.pem"
     algorithm: str = 'RS256'
-    access_token_expire_minutes: int = 2
-    refresh_token_expire_days: int = 7
+    access_token_expire_minutes: int = 3600
+    refresh_token_expire_days: int = 30
 
 
 class Bot(BaseModel):
