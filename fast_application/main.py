250d4655f9d7a6468885675e5c2893d4ef801fb2
Romberto <korhoff@mail.ru>
2025-07-28 12:01:22 +0400

    merge brod-test

--- fast_application/main.py
+++ fast_application/main.py
@@ -19,9 +19,7 @@ app_main = FastAPI(lifespan=lifespan)
 
 app_main.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://romberto.github.io"
-    ],
+    allow_origins=["https://romberto.github.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -32,8 +30,5 @@ app_main.include_router(api_router)
 
 if __name__ == "__main__":
     uvicorn.run(
-        "main:app_main",
-        host=settings.run.host,
-        port=settings.run.port,
-        reload=True
+        "main:app_main", host=settings.run.host, port=settings.run.port, reload=True
     )
